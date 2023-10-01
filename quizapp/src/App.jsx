d9307923e5e5c89cae594a2b114611c305391292
JohanaGikonyo <johanagikonyo552@gmail.com>
2023-10-01 15:21:58 +0300

    new7

--- quizapp/src/App.jsx
+++ quizapp/src/App.jsx
@@ -37,7 +37,7 @@ useEffect(()=>{
       <Routes>
         <Route path='/' element={<About/>} />
         <Route path='/contacts' element={<Contact/>}/>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/home' element={<Website/>}/>
       </Routes>
       
       <div className='back'>
