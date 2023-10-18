93dc0928d6014717a7eadcdfce5ad040c5e2c198
JohanaGikonyo <johanagikonyo552@gmail.com>
2023-10-18 10:57:54 +0300

    twitter

--- quizapp/src/component/Footer.jsx
+++ quizapp/src/component/Footer.jsx
@@ -15,7 +15,8 @@ function Footer() {
                 <a href='https://wa.me/+254740550484' target='_blank'> <em><box-icon name='whatsapp' type='logo' ></box-icon>Whatsapp</em></a>
                 <a href='https://www.facebook.com/johanagikonyo552@gmail.com' target='_blank'> <em><box-icon name='facebook' type='logo' ></box-icon>Facebook</em></a>
                 <a href='https://www.twitter.com/@johanagikonyo552@gmail.com' target='_blank'>  <em><box-icon name='twitter' type='logo' ></box-icon>Twitter</em></a>
-                <a href='https://www.instagram.com/johanagikonyo552@gmail.com' target='_blank'><em><box-icon name='instagram' type='logo' ></box-icon>Instagram</em></a></div>
+                <a href='https://www.instagram.com/johanagikonyo552@gmail.com' target='_blank'><em><box-icon name='instagram' type='logo' ></box-icon>Instagram</em></a>
+                </div>
                 <em>Copywrite@jgk{new Date().getFullYear()}</em>
         </footer> 
         
