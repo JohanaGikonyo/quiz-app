0969cc7bb107f23286fc5be32fa320484a2eeab2
JohanaGikonyo <johanagikonyo552@gmail.com>
2023-10-18 10:34:27 +0300

    facebook

--- quizapp/src/component/Footer.jsx
+++ quizapp/src/component/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
                 </ul>
                 <div className='icons'>
                 <a href='https://wa.me/+254740550484' target='_blank'> <em><box-icon name='whatsapp' type='logo' ></box-icon>Whatsapp</em></a>
-                <em><box-icon name='facebook' type='logo' ></box-icon>Facebook</em>
+                <a href='https://www.facebook.com/johanagikonyo552@gmail.com' target='_blank'> <em><box-icon name='facebook' type='logo' ></box-icon>Facebook</em></a>
                 <em><box-icon name='twitter' type='logo' ></box-icon>Twitter</em>
                 <em><box-icon name='instagram' type='logo' ></box-icon>Instagram</em></div>
                 <em>Copywrite@jgk{new Date().getFullYear()}</em>
