90cc1d79c68218b85ae74a7e9dfb42bf84cd4b44
JohanaGikonyo <johanagikonyo552@gmail.com>
2023-10-31 20:58:29 +0300

    nav

--- quizapp/src/component/Footer.jsx
+++ quizapp/src/component/Footer.jsx
@@ -6,10 +6,9 @@ function Footer() {
     <div>
     <footer>                      
         <ul>
-               <li><NavLink to='/'>About</NavLink></li> 
-               <li> <NavLink  to='/contacts'>Contact</NavLink></li> 
-               <li><NavLink  to='/home'>Home</NavLink></li> 
-               
+       <li><a href='about'>About</a></li> 
+              <li><a href='home'>Home</a></li> 
+              <li><a href='contacts'>contacts</a></li> 
                 </ul>
                 <div className='icons'>
                 <a href='https://wa.me/+254740550484' target='_blank'> <em><box-icon name='whatsapp' type='logo' ></box-icon>Whatsapp</em></a>
